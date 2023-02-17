0e86dd9df6337b481acad09c9da938b56151ca4e
acedev003 <acedeveloper003@gmail.com>
2023-02-17 23:15:56 +0530

    Code update|mean instead of mode|3s improvement

--- src/cinetone/__init__.py
+++ src/cinetone/__init__.py
@@ -1,12 +1,14 @@
 import imageio as iio
 import argparse
-from PIL import Image   
+from PIL import Image
 import numpy as np
 import os
 
+
 def main():
-    parser = argparse.ArgumentParser(prog = 'cinetone',description = '',epilog = '')
-    parser.add_argument("video_file",help="Path to the video file to be decoded")
+    parser = argparse.ArgumentParser(
+        prog='cinetone', description='', epilog='')
+    parser.add_argument("video_file", help="Path to the video file to be decoded")
     args = parser.parse_args()
 
     if not os.path.isfile(args.video_file):
@@ -14,18 +16,24 @@ def main():
         return
 
     pix_list = []
-    rowcnt   = 0
-    colscnt  = 0
+    rowcnt = 0
+    colscnt = 0
+    channels = 3
 
     for frame in iio.imiter(args.video_file):
 
         if rowcnt == 0 or colscnt == 0:
-            rowcnt  = frame.shape[0]
+            rowcnt = frame.shape[0]
             colscnt = frame.shape[1]
 
-        img = Image.fromarray(frame)
-        pix_list.append(img.resize((1, 1), resample=0).getpixel((0,0)))
-    
+        rgb = [0, 0, 0]
+
+        flattened_img = np.resize(frame, (rowcnt*colscnt, 3))
+        for index in range(channels):
+            rgb[index] = int(flattened_img[:,index].mean())
+
+        pix_list.append((rgb[0], rgb[1], rgb[2]))
+
     im = Image.new(mode="RGB",size=(len(pix_list),1))
     im.putdata(pix_list)
     im = im.resize((480,360),resample=Image.Resampling.BICUBIC)
