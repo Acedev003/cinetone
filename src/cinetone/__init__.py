cc65bda41ac9d4eccb690f437a4afa1dc8fb3d46
acedev003 <acedeveloper003@gmail.com>
2023-02-18 15:42:09 +0530

    Add output and frameskip arguments

--- src/cinetone/__init__.py
+++ src/cinetone/__init__.py
@@ -4,10 +4,19 @@ from PIL import Image
 import numpy as np
 import os
 
-
 def main():
-    parser = argparse.ArgumentParser(
-        prog='cinetone', description='', epilog='')
+
+    pix_list  = []
+    rowcnt    = 0
+    colscnt   = 0
+    channels  = 3
+    skipcount = 60
+
+    outfile  = "out.png"
+
+    parser = argparse.ArgumentParser(prog='cinetone', description='A python CLI tool for generating a color palette from a given input video', epilog='')
+    parser.add_argument("-o","--output",help="Places the output in the specified file")
+    parser.add_argument("-sc","--skipcount",help="Specifies the frames to skip while processing. Default value is 50",type=int)
     parser.add_argument("video_file", help="Path to the video file to be decoded")
     args = parser.parse_args()
 
@@ -15,20 +24,21 @@ def main():
         print("Given path is not a file!")
         return
 
-    pix_list = []
-    rowcnt = 0
-    colscnt = 0
-    channels = 3
+    if(args.output):
+        outfile = args.output
+
+    if(args.skipcount):
+        skipcount = args.skipcount
 
     for index,frame in enumerate(iio.imiter(args.video_file)):
-        if(index%50 != 0):
+        if( index % skipcount != 0):
             continue
 
         if rowcnt == 0 or colscnt == 0:
             rowcnt = frame.shape[0]
             colscnt = frame.shape[1]
 
-        rgb = [0, 0, 0]
+        rgb = [0] * channels
 
         flattened_img = np.resize(frame, (rowcnt*colscnt, 3))
         for index in range(channels):
@@ -39,8 +49,4 @@ def main():
     im = Image.new(mode="RGB",size=(len(pix_list),1))
     im.putdata(pix_list)
     im = im.resize((480,360),resample=Image.Resampling.NEAREST)
-    im.save("out.png")
-
-    
-
-main()
+    im.save(outfile)
